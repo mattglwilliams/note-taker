840461ec7d15ea7aae33c8ef2406472af76a1afd
Matthew Williams <matthewwilliams@matts-mbp.home>
2022-01-21 18:27:31 +0000

    Moced my uuid locally as all notes were getting the same id

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,48 +7,48 @@ const { randomUUID } = require("crypto");
 const id = randomUUID();
 
 router.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (error, data) => {
+  fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (error, data) => {
+    if (error) {
+      res.status(404).send(error);
+    } else {
+      res.send(JSON.parse(data));
+    }
+  });
+});
+
+router.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = { title, text, id };
+
+    fs.readFile("./db/db.json", "utf8", (error, data) => {
       if (error) {
-        res.status(404).send(error);
+        console.log(error);
       } else {
-        res.send(JSON.parse(data));
+        const parsedNote = JSON.parse(data);
+        parsedNote.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(parsedNote), (error) => {
+          error ? console.log(error) : res.send("Success");
+        });
       }
     });
-  });
-  
-router.post("/api/notes", (req, res) => {
-    const { title, text } = req.body;
-
-    if (title && text) {
-        const newNote = { title, text, id };
-
-        fs.readFile("./db/db.json", "utf8", (error, data) => {
-        if (error) {
-            console.log(error);
-        } else {
-            const parsedNote = JSON.parse(data);
-            parsedNote.push(newNote);
-            fs.writeFile("./db/db.json", JSON.stringify(parsedNote), (error) => {
-            error ? console.log(error) : res.send("Success");
-            });
-        }
-        });
-    }
+  }
 });
-  
+
 router.delete("/api/notes/:id", (req, res) => {
-    fs.readFile("./db/db.json", (error, data) => {
-        if (error) {
-        res.send(error);
-        } else {
-        const notes = JSON.parse(data);
-        const newNotesArr = notes.filter((note) => note.id !== req.params.id);
-        fs.writeFile("./db/db.json", JSON.stringify(newNotesArr), (error) => {
-            error ? console.log(error) : res.send("Success");
-        });
-        console.log(req.params.id);
-        }
-    });
+  fs.readFile("./db/db.json", (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      const notes = JSON.parse(data);
+      const newNotesArr = notes.filter((note) => note.id !== req.params.id);
+      fs.writeFile("./db/db.json", JSON.stringify(newNotesArr), (error) => {
+        error ? console.log(error) : res.send("Success");
+      });
+      console.log(req.params.id);
+    }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const apiRoutes = require("./routes/api-routes");
-const htmlRoutes = require("./routes/html-routes");
+const apiRoutes = require("./routes/api-routes")
+const htmlRoutes = require("./routes/html-routes")
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.port || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.use(apiRoutes);
-app.use("/", htmlRoutes);
+app.use(apiRoutes)
+app.use("/", htmlRoutes)
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}.`);
